dcae111f73724168274fc0c958ef304435fe8d5d
Matthew Jung <jmfqcrf@jmfamily.com>
2026-08-27 10:44:01 -0400

    deleted program

--- codeJamBlackjackcli/Program.cs
+++ codeJamBlackjackcli/Program.cs
@@ -1,1 +1,13 @@
-﻿Console.WriteLine("Hello, World!");
+// Blackjack Brawl
+// Standard blackjack rules, but instead of betting money you're betting HP.
+// Beat the Evil Dealer of the Garnet & Gold to zero HP before it does the same to you.
+
+namespace BlackjackBrawl;
+
+class Program
+{
+    static void Main()
+    {
+        new Game().Run();
+    }
+}
